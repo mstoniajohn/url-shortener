b05855d019d4227f15ae69418acf9a833b42e7d0
github username <github email address>
2021-04-10 07:43:21 -0400

    added morgan

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 app.use(express.json({ extended: false }));
-
+app.use(morgan('combined'));
 app.use(express.static('public'));
 // app.use(ignoreFavicon);
 
